11d7fa6a7fba506945639c53e1eaf8a0511c0851
Garry Newby <newby1979@gmail.com>
2026-01-05 13:39:31 +0000

    added the routes folder

---
+++ routes/taskRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/taskController.js";
+
+const router = express.Router();
+
+// GET all tasks
+router.get("/", getTasks);
+
+// CREATE a new task
+router.post("/", createTask);
+
+// UPDATE a task
+router.put("/:id", updateTask);
+
+// DELETE a task
+router.delete("/:id", deleteTask);
+
+export default router;
